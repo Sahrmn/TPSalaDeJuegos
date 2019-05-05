71974089c2f48851f399550df343ce08c440d679
Sahrmn <samy32m@gmail.com>
2019-05-05 18:50:50 -0300

    anda tateti

--- src/app/clases/tateti.ts
+++ src/app/clases/tateti.ts
@@ -1,7 +1,13 @@
+
 export class Tateti {
-	tablero;
-	turno: number;
-    criterioDeGanador = [
+	  tablero;
+	  IA_PLAYER = 1;
+  	USER_PLAYER = 2;
+  	result = "";
+    ganoUser: boolean = false;
+    ganoUI: boolean = false;
+    empate: boolean = false;
+    /*criterioDeGanador = [
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9],
@@ -10,61 +16,177 @@ export class Tateti {
             [3, 6, 9],
             [1, 5, 9],
             [3, 5, 7]
-    ];
+    ];*/
 
-	//nuevo tablero
-	getInitialBoard() {
-		return [
-		[null,null,null],
-		[null,null,null],
-		[null,null,null]];
-	}
-
-    /*verificarSiHayGanador() {
-    	let respuesta: boolean = false;
-        for(let i of this.criterioDeGanador){
-            let a = this.tablero[i[0]];
-            let b = this.tablero[i[1]];
-            let c = this.tablero[i[2]];
-            if(a !== " " && a === b && a === c){
-                respuesta = true;
-            }
-        }
-        return respuesta;
+    constructor(){
+      this.tableroVacio();
     }
 
-    setTurno(){
-        if(this.turno == 0){
-            this.turno = 1
-        }else{
-            this.turno = 0
-    	}
-	}*/
+  // Vacia el tablero
+  tableroVacio() {
+    this.tablero = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+    ];
+  }
 
-	// Genera el tablero en la página
-	public generateUIBoard() {
-		let html = "";
-		for (var i = 0; i < 9; i++) {
-			html += ('<li><div class="cell"></div></li>');
+  // Juega la máquina
+  iaMoves() {
+    if(this.ganoUser == false && this.empate == false)
+    {
+      var options = this.getFreePositions();
+      if(options.length != 0)
+      {
+          
+        var optionIndex = this.randomIntTo(options.length-1);
+        var coords = options[optionIndex];
+
+        // marcamos la casilla
+        this.checkCell(coords[0], coords[1], this.IA_PLAYER);
+
+        var w = this.winner();
+        if (w == 1 || w == 2 || w == -1) {
+          this.finish(w);
+          return true;
+        }
+        /*else {
+          this.userMoves();
+        }*/
+      }
+    }
+  }
+
+  restart(){
+    this.ganoUser = false;
+    this.empate = false;
+    this.ganoUI = false;
+    this.tableroVacio();
+    this.result = "";
+  }
+
+  // Juega el jugador humano.
+	userMoves(number1, number2) {
+
+		var row = number1;
+		var col = number2;
+    
+		this.checkCell(row, col, this.USER_PLAYER);
+		var w = this.winner();
+    //console.log(w);
+		if (w == 1 || w == 2 || w == -1) {
+		  this.finish(w);
+      return true;
 		}
-		return html;
+		else {
+      //console.log("ya casi juego la ia");
+          //console.log(this.ganoUser);
+        if(this.ganoUser == false && this.empate == false)
+        {
+          //console.log("ya casi juego la ia -> gano? " + this.ganoUser + " - empate? " + this.empate);
+      		setTimeout(() => { 
+            this.iaMoves(); 
+          }, 1000);
+        }
+  	}
 	}
 
-	// Actualiza la página para reflejar el estado actual del
-	// tablero
-	/*updateUI(board) {
-		for (var row = 0; row < 3; row++) {
-			for (var col = 0; col < 3; col++) {
-				var index = ((row * 3) + col) + 1;
-				if (board[row][col] != null) {
-				$('li:nth-child('+index+') .cell')
-				.addClass('marked')
-				.addClass('player-' + board[row][col]);
-				}
-			}
-		}
-	}*/
+
+
+ finish(winner) {
+  //console.log("winner->" + winner);
+    if (winner == this.IA_PLAYER) {
+      this.result = "Perdiste contra un jugador Random!";
+      this.ganoUI = true;
+    } else if (winner == this.USER_PLAYER) {
+      this.result = "Ganaste!";
+      this.ganoUser = true;
+    } else {
+      this.result = "Es un empate!";
+      this.empate = true;
+    }
+    console.log(this.result);
+  }
+
+
+// Retorna un int random entre 0 y n inclusive.
+  randomIntTo(n) {
+  	let num = Math.floor((Math.random()*n)+1); 
+    return num;
+  }
+
+// Marca la casilla 
+  checkCell(row, col, player) {
+    this.tablero[row][col] = player;
+    //console.log(this.tablero);
+  }
+
+
+// Devuelve las coordenadas como [fila,columna] de las posiciones libres del tablero
+  getFreePositions() {
+    var freePositions = new Array();
+    for (var row = 0; row < 3; row++) {
+      for (var col = 0; col < 3; col++) {
+        if (this.tablero[row][col] == 0) {
+          let arr = [row, col];
+          //console.log(arr);
+          freePositions.push(arr);
+        }
+      }
+    }
+    //console.log(freePositions);
+    return freePositions;
+  }
+
+
+// Retorna 1 si no hay ganador, -1 si hay empate.
+  winner() {
+    // verificar columnas
+    for (var c = 0; c <= 2; c++) {
+      if (this.tatetiCol(c)) return this.tablero[0][c];
+    }
+    // verificar filas
+    for (var r = 0; r <= 2; r++) {
+      if (this.tatetiRow(r)) return this.tablero[r][0];
+    }
+    // verificar diagonales
+    if (this.tatetiDiagonals()) return this.tablero[1][1];
+    // verificar empate
+    let posiciones = this.getFreePositions();
+    if (posiciones.length == 0) return -1; // empate
+    //console.log("posiciones disponibles: " +posiciones.length);
+    return 0;
+  }
+
+// True si las celdas de la columna indicada son iguales y no nulas
+  tatetiCol(col) {
+    return this.tablero[0][col] != 0
+    && (this.tablero[0][col] == this.tablero[1][col] && this.tablero[1][col] == this.tablero[2][col]);
+  }
+
+// True si las celdas de la fila indicada son iguales y no nulas
+  tatetiRow(row) {
+    return this.tablero[row][0] != 0
+    && (this.tablero[row][0] == this.tablero[row][1] && this.tablero[row][1] == this.tablero[row][2]);
+  }
+
+// True si alguna de las diagonales tienen valores iguales y no nulos
+  tatetiDiagonals() {
+    return (this.tablero[0][0] != 0 && (this.tablero[0][0] == this.tablero[1][1] && this.tablero[1][1] == this.tablero[2][2]))
+    || (this.tablero[0][2] != 0 && (this.tablero[0][2] == this.tablero[1][1] && this.tablero[1][1] == this.tablero[2][0]));
+  }
+
+
+  verificarGanador(){
+    if(this.ganoUser == false && this.ganoUI == false && this.empate == false)
+      return -1;
+    if(this.ganoUser)
+      return this.USER_PLAYER;
+    else if(this.ganoUI)
+      return this.IA_PLAYER;
+    else if(this.empate)
+      return 0;
+  }
 
 	
-
 }
--- src/app/componentes/tateti/tateti.component.ts
+++ src/app/componentes/tateti/tateti.component.ts
@@ -7,23 +7,105 @@ import { Tateti } from '../../clases/tateti';
   styleUrls: ['./tateti.component.css']
 })
 export class TatetiComponent implements OnInit {
-
-  IA_PLAYER = 1;
-  USER_PLAYER = 2;
   nuevoJuego: Tateti;
+  tableroVista = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+    ];
   tablero;
+  ganoU: boolean = false;
+  ganoUI: boolean = false;
+  empate: boolean = false;
 
   constructor() { 
   	this.nuevoJuego = new Tateti();
-  	this.generarTablero();
   }
 
   ngOnInit() {
   }
 
-  generarTablero(){
-  	this.tablero = this.nuevoJuego.generateUIBoard();
-  	//console.log(this.tablero);
+  nuevo(){
+    this.nuevoJuego.restart();
+    this.ganoU = false;
+    this.ganoUI = false;
+    this.empate = false;
+    this.tablero = this.nuevoJuego.tablero;
+    this.tableroVista = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+    ];
   }
+
+  tildar(number1, number2){
+    if(this.tableroVista[number1][number2] == "")
+    {
+      if(this.ganoU != true && this.empate != true && this.ganoUI != true)
+      {
+        this.nuevoJuego.userMoves(number1, number2);
+        this.tablero = this.nuevoJuego.tablero;
+        //console.log(this.tablero);
+        this.tableroVista[number1][number2] = "x";
+        
+        //verifico si alguien ganó
+        this.verificoGanador();
+
+        setTimeout(() => { 
+            this.tablero = this.nuevoJuego.tablero;
+            //console.log(this.tablero);
+            this.verificarTildar();
+            
+          }, 1500);
+        
+      }
+    }
+  }
+
+  verificarTildar(){
+    for (var row = 0; row < 3; row++) {
+      for (var col = 0; col < 3; col++) {
+        if(this.tablero[row][col] == 1)
+        {
+          this.tableroVista[row][col] = "o";
+        }
+        else if(this.tablero[row][col] == 2)
+        {
+          this.tableroVista[row][col] = "x";
+        }
+      }
+    }
+  }
+
+  verificoGanador(){
+    //console.log("Estoy verificando si alguien ganó...");
+    let retorno = this.nuevoJuego.verificarGanador();
+        switch(retorno)
+        {
+          case -1:
+          break;
+          case 1:
+            this.ganoUI = true;
+          break;
+          case 2:
+            this.ganoU = true;
+          break;
+          case 0:
+            this.empate = true;
+          break;
+        }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 }
